1ae6298e3fc57ccaa2e4c2ecf65d7a687f810b7b
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-07-22 02:36:23 +0600

    validation added to book routes

--- src/app/modules/books/book.route.ts
+++ src/app/modules/books/book.route.ts
@@ -1,13 +1,31 @@
 import express from "express";
 import bookController from "./book.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import {
+    addReviewZodValidationSchema,
+    updateBookZodValidationSchema,
+    createBookZodValidationSchema,
+} from "./book.validation";
 
 const router = express.Router();
 
-router.get("/:id", bookController.getSingleBook)
-router.patch("/:id", bookController.updateBook)
-router.delete("/:id", bookController.deleteBook)
-router.post("/addReview/:id", bookController.postReview)
-router.get("/", bookController.getAllBooks)
-router.post("/add-book", bookController.postBook)
+router.get("/:id", bookController.getSingleBook);
+router.patch(
+    "/:id",
+    validateRequest(updateBookZodValidationSchema),
+    bookController.updateBook
+);
+router.delete("/:id", bookController.deleteBook);
+router.post(
+    "/addReview/:id",
+    validateRequest(addReviewZodValidationSchema),
+    bookController.postReview
+);
+router.get("/", bookController.getAllBooks);
+router.post(
+    "/add-book",
+    validateRequest(createBookZodValidationSchema),
+    bookController.postBook
+);
 
 export default router;
