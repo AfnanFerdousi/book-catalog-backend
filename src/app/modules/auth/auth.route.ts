0bf527c9c4c4a65985d6c62d3c5909a678c630f4
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-07-22 12:00:36 +0600

    validation added

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import authController from './auth.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { loginZodValidationSchema, refreshZodValidationSchema } from './auth.validation';
 
 const router = express.Router();
 
 router.post("/signup", authController.createUser);
-router.post("/login", authController.loginUser);
-router.post("/refresh", authController.refreshToken);
+router.post("/login", validateRequest(loginZodValidationSchema), authController.loginUser);
+router.post("/refresh", validateRequest(refreshZodValidationSchema),authController.refreshToken);
 router.patch("/add-to-wish/:id", authController.addToWishList);
 router.get("/wishlist/:id", authController.getWishList);
 
